69c84d302ecde7ecb1ad0dd61fe71704a3c9b9b1
Bernardo Bortot Spiazzi <bernardo.spiazzi@ciss.com.br>
2024-10-27 21:15:14 -0300

    Commit Inicial

---
+++ Angular + Spring/durcSys/src/main/java/br/com/durcsys/DurcSysApplication.java
@@ -0,0 +1,13 @@
+package br.com.durcsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DurcSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DurcSysApplication.class, args);
+	}
+
+}
